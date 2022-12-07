3f2db33980467b3624a1d7442d967ab8ccecd7ed
Anthony Thomasson <athomasson.93@gmail.com>
2022-12-06 23:28:09 -0700

    Day 7 ✅

---
+++ src/Day7/solution.test.ts
@@ -0,0 +1,53 @@
+import fs from 'fs'
+import path from 'path'
+import { solutionPartOne } from './solution_part1'
+import { solutionPartTwo } from './solution_part2'
+
+describe('Day 7', () => {
+    describe('Part One', () => {
+        it('If we provide the example input, then we should get the correct result', () => {
+            expect(
+                solutionPartOne(
+                    fs.readFileSync(
+                        path.join(__dirname, '/input/example_input.txt'),
+                        {
+                            encoding: 'utf8',
+                        }
+                    )
+                )
+            ).toBe(95437)
+        })
+        it('OFFICIAL INPUT', () => {
+            expect(
+                solutionPartOne(
+                    fs.readFileSync(path.join(__dirname, '/input/input.txt'), {
+                        encoding: 'utf8',
+                    })
+                )
+            ).toBe(1390824)
+        })
+    })
+    describe('Part Two', () => {
+        it('If we provide the example input, then we should get the correct result', () => {
+            expect(
+                solutionPartTwo(
+                    fs.readFileSync(
+                        path.join(__dirname, '/input/example_input.txt'),
+                        {
+                            encoding: 'utf8',
+                        }
+                    )
+                )
+            ).toBe(24933642)
+        })
+        it('OFFICIAL INPUT', () => {
+            expect(
+                solutionPartTwo(
+                    fs.readFileSync(path.join(__dirname, '/input/input.txt'), {
+                        encoding: 'utf8',
+                    })
+                )
+            ).toBe(7490863)
+        })
+    })
+})
---
+++ src/Day7/solution_part2.ts
@@ -0,0 +1,215 @@
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/lib/function'
+import * as O from 'fp-ts/Option'
+import * as ARO from 'fp-ts/ReadonlyArray'
+import * as S from 'fp-ts/string'
+
+interface LineEntry {
+    type: 'cmd' | 'file' | 'dir'
+}
+
+interface Cmd extends LineEntry {
+    cmdType: 'cd' | 'ls'
+    target: '/' | '..' | string
+}
+
+interface Dir extends LineEntry {
+    id: number
+    name: string
+    parent: Dir | null
+    children: Array<Dir | File>
+}
+
+interface File extends LineEntry {
+    id: number
+    name: string
+    size: number
+    parent: Dir | null
+}
+
+interface ProcessingContext {
+    curDir: Dir | null
+    tree: Array<Dir | File>
+    dirSizes: Map<string, number>
+}
+
+export const solutionPartTwo = (input: string): number => {
+    const rootDir: Dir = {
+        id: Math.floor(Math.random() * 1000),
+        type: 'dir',
+        name: '/',
+        parent: null,
+        children: [],
+    }
+    const startingContext: ProcessingContext = {
+        curDir: rootDir,
+        tree: [rootDir],
+        dirSizes: new Map(),
+    }
+
+    const orderedDirSizes = pipe(
+        input,
+        S.split('\n'),
+        ARO.reduce<string, E.Either<Error, ProcessingContext>>(
+            E.of(startingContext),
+            (acc, line) => {
+                const processedContext = pipe(
+                    E.Do,
+                    E.bind('acc', () => acc),
+                    E.bind('line', () => E.of(line)),
+                    processLine
+                )
+                return processedContext
+            }
+        ),
+        E.map((ctx) => {
+            return ctx.dirSizes
+        }),
+        E.chain((dirsMap) => E.of(Array.from(dirsMap.values()))),
+        E.chain((dirValues) => E.of(dirValues.sort((a, b) => a - b))),
+        E.getOrElse(() => [] as number[])
+    )
+
+    const currSpaceTaken = orderedDirSizes[orderedDirSizes.length - 1]
+    const spaceAvailable = 70000000 - currSpaceTaken
+    for (const dirSize of orderedDirSizes) {
+        if (spaceAvailable + dirSize > 30000000) {
+            return dirSize
+        }
+    }
+
+    return 0
+}
+
+const processLine = (
+    ctx: E.Either<
+        Error,
+        {
+            acc: ProcessingContext
+            line: string
+        }
+    >
+): E.Either<Error, ProcessingContext> => {
+    return pipe(
+        ctx,
+        E.chain(({ acc, line }) => {
+            const processedAcc = pipe(
+                line,
+                parseLine,
+                E.map((lineEntry) => {
+                    if (lineEntry.type === 'cmd') {
+                        const cmd = lineEntry as Cmd
+                        if (cmd.cmdType === 'cd') {
+                            return processCd(cmd, acc)
+                        }
+                        if (cmd.cmdType === 'ls') {
+                            return processLs(cmd, acc)
+                        }
+                        return acc
+                    }
+                    const node = lineEntry as File | Dir
+                    return addSystemNode(node, acc)
+                }),
+                E.getOrElse(() => acc)
+            )
+            return E.of({ processedAcc, line })
+        }),
+        E.chain((ctx) => E.of(ctx.processedAcc))
+    )
+}
+
+const addSystemNode = (
+    node: File | Dir,
+    ctx: ProcessingContext
+): ProcessingContext => {
+    ctx.curDir?.children.push(node)
+    node.parent = ctx.curDir
+    if (node.type === 'file') {
+        const file = node as File
+        const fileSize = parseInt(file.size.toString())
+        if (ctx.curDir != null) {
+            updateFileSize(ctx.curDir, ctx.dirSizes, fileSize)
+        }
+    } else {
+        ctx.dirSizes.set(node.name + node?.id.toString(), 0)
+    }
+    return ctx
+}
+
+const updateFileSize = (
+    node: Dir,
+    dirSizes: Map<string, number>,
+    size: number
+): void => {
+    const currentSize = dirSizes.get(node.name + node?.id.toString()) ?? 0
+    dirSizes.set(node.name + node?.id.toString(), size + currentSize)
+    if (node.parent != null) {
+        updateFileSize(node.parent, dirSizes, size)
+    }
+}
+
+const processLs = (cmd: Cmd, ctx: ProcessingContext): ProcessingContext => {
+    return ctx
+}
+
+const processCd = (cmd: Cmd, ctx: ProcessingContext): ProcessingContext => {
+    if (cmd.target === '/') {
+        if (ctx.tree[0] != null) {
+            const root = ctx.tree[0] as Dir
+            ctx.curDir = root
+        }
+    } else if (cmd.target === '..') {
+        if (ctx.curDir != null) {
+            ctx.curDir = ctx.curDir.parent
+        }
+    } else {
+        ctx.curDir = ctx.curDir?.children.find((child) => {
+            return child.type === 'dir' && child.name === cmd.target
+        }) as Dir
+    }
+    return ctx
+}
+
+const parseLine = (line: string): E.Either<Error, LineEntry> =>
+    pipe(
+        /^\$\s+([^\s]+)\s*(.*)$/.exec(line),
+        O.fromNullable,
+        O.map<RegExpExecArray, LineEntry>(([, cmdType, target]) => {
+            return {
+                type: 'cmd',
+                cmdType,
+                target,
+            }
+        }),
+        O.alt<LineEntry>(() =>
+            pipe(
+                /^dir\s(.+)$/.exec(line),
+                O.fromNullable,
+                O.map<RegExpExecArray, LineEntry>(([, name]) => {
+                    return {
+                        id: Math.floor(Math.random() * 1000),
+                        type: 'dir',
+                        name,
+                        children: [],
+                    }
+                })
+            )
+        ),
+        O.alt<LineEntry>(() =>
+            pipe(
+                /^(\d+)\s(.+)$/.exec(line),
+                O.fromNullable,
+                O.map<RegExpExecArray, LineEntry>(([, size, name]) => {
+                    return {
+                        id: Math.floor(Math.random() * 1000),
+                        type: 'file',
+                        name,
+                        size,
+                    }
+                })
+            )
+        ),
+        E.fromOption(() => {
+            return new Error(`unknown instruction line: "${line}"`)
+        })
+    )
